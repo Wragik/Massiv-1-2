e6618df7c05320808ed5b7637be0f7e7484378df
Wragik <man.bick@yandex.ru>
2022-08-24 12:37:17 +0700

    massiv
    massiv

---
+++ Mass1/Mass1/Mass1.cpp
@@ -0,0 +1,40 @@
+﻿// Mass1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251); // Починка русского языка
+    setlocale(LC_ALL, "rus");
+    int a, i, p, n, max=0;
+    cout << "Введите кол-во ";
+    cin >> a;
+    string* z = new string[a];
+    string f,maximus;
+    for (i = 0; i < a; i++)
+    {
+        cout << "Введите слово ";
+        cin >> z[i];
+        f = z[i];
+        p = f.length();
+        if (p > max)
+        {
+            max = p;
+            maximus = z[i];
+        }
+
+
+    }
+    delete[]z;
+    cout << "Самое длинное слово из введённых: " << maximus;
+    
+    
+    
+}
+
+
---
+++ Mass2/Mass2/Mass2.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+#include<ctime>;
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL,"rus");
+    int  maxm = -10, minp = 10, k = 0, o = 0;
+    int* z = new int[20];
+    srand(time(NULL));
+    cout << "Основной массив ";
+    for (int i = 0; i < 20; i++)
+    {
+        
+        z[i] = rand()%20-10;
+        cout << z[i] << " ";
+        if (z[i] < 0 && maxm < z[i])
+        {
+            maxm = z[i];
+            k = i;
+        }
+        if (z[i] > 0 && minp > z[i])
+        {
+            minp = z[i];
+            o = i;
+        }
+        
+    }
+
+    cout << "\n" << "Изменённый массив ";
+    for (int i = 0; i < 20; i++)
+    {
+        z[k] = minp;
+        z[o] = maxm;
+        cout << z[i] << " ";
+    }
+    delete[]z;
+}
+
